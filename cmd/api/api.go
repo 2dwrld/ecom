07c918c318a18400ba61dac32920d4577a4dc8da
2dwrld <aintstressedanymore@gmail.com>
2024-06-27 20:37:00 +0300

    connection on a port ':8080'

---
+++ cmd/api/api.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"database/sql"
+	"github.com/2dwrld/ecom/service/user"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := http.NewServeMux()
+	subrouter := http.NewServeMux()
+	subrouter.Handle("/api/v1/", http.StripPrefix("/api/v1", router))
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
